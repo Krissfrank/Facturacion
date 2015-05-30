bbfeb7bb062bf697cefe9db6920137258a7743b9
Cristian <krissfrank@gmail.com>
2015-05-30 14:13:30 -0600

    Nueva Actualizacion 30/05/2015

--- src/main/java/com/mycompany/facturacion/controllers/VoucherController.java
+++ src/main/java/com/mycompany/facturacion/controllers/VoucherController.java
@@ -16,8 +16,44 @@ public class VoucherController {
     }
     
     public static Object saveForm(Request rq, Response rs){
-        String user = rq.queryParams("user");
+        String Tnombre = rq.queryParams("Tnombre");
+        String Trfc= rq.queryParams("Trfc"); 
+        String Ttaxation= rq.queryParams("Ttaxation"); 
+        String Tcolonia= rq.queryParams("Tcolonia"); 
+        String Tcalle= rq.queryParams("Tcalle"); 
+        String Tinterior= rq.queryParams("Tinterior"); 
+        String Texterior= rq.queryParams("Texterior"); 
+        String Tlocalidad= rq.queryParams("Tlocalidad"); 
+        String Treferencia= rq.queryParams("Treferencia"); 
+        String Tpais= rq.queryParams("Tpais"); 
+        String Testado= rq.queryParams("Testado"); 
+        String Tciudad= rq.queryParams("Tciudad"); 
+        String Tcp= rq.queryParams("Tcp");
+        String Rnombre= rq.queryParams("Rnombre");
+        String Rrfc= rq.queryParams("Rrfc");
+        String Rtaxation= rq.queryParams("Rtaxation");
+        String Rcolonia= rq.queryParams("Rcolonia");
+        String Rcalle= rq.queryParams("Rcalle");
+        String Rinterior= rq.queryParams("Rinterior");
+        String Rexterior= rq.queryParams("Rexterior");
+        String Rlocalidad= rq.queryParams("Rlocalidad");
+        String Rreferencia= rq.queryParams("Rreferencia");
+        String Rpais= rq.queryParams("Rpais");
+        String Restado= rq.queryParams("Restado");
+        String Rciudad= rq.queryParams("Rciudad");
+        String Rcp= rq.queryParams("Rcp");
+        String Rmail= rq.queryParams("Rmail");        
+        String Vserie= rq.queryParams("Vserie");
+        String Vfolio= rq.queryParams("Vfolio");
+        String Vdivisa= rq.queryParams("Vdivisa");
+        String Vtipodiv= rq.queryParams("Vtipodiv");
+        String Vmetpago= rq.queryParams("Vmetpago");
+        String Vtipopago= rq.queryParams("Vtipopago");
+        String Vcondition= rq.queryParams("Vcondition");
+        String Vplaceex= rq.queryParams("Vplaceex");
+        String Vkindvouch= rq.queryParams("Vkindvouch");
         
         return null;
     }
+    
 }
