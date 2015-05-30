cec39f15fea88fdef6068c3cb868d14dea0ac0e1
Abelardo <Abelardo@148.229.22.57>
2015-05-29 20:25:25 -0600

    > Login, Formulario

--- src/main/java/com/mycompany/facturacion/Facturacion.java
+++ src/main/java/com/mycompany/facturacion/Facturacion.java
@@ -3,13 +3,9 @@ package com.mycompany.facturacion;
 import com.github.luischavez.database.Database;
 import com.github.luischavez.database.configuration.ProjectSource;
 import com.github.luischavez.database.configuration.XMLBuilder;
-import com.github.luischavez.database.link.Row;
-import com.github.luischavez.database.link.RowList;
 import com.mycompany.facturacion.controllers.IndexController;
 import com.mycompany.facturacion.controllers.UserController;
-import java.util.HashMap;
-import java.util.Map;
-import spark.ModelAndView;
+import com.mycompany.facturacion.controllers.VoucherController;
 import static spark.Spark.*;
 import spark.template.jade.JadeTemplateEngine;
 
@@ -22,34 +18,19 @@ public class Facturacion {
     public static void main(String[] args) {
         Database.load(new XMLBuilder(), new ProjectSource("/database.xml"));
         port(80);
-        
-
-      
 
         // The hello.jade template file is in the resources/templates directory
-        get("/index", IndexController::index , new JadeTemplateEngine());
-        get("/login",UserController::login , new JadeTemplateEngine());
-        post("/login", UserController::doLogin );
-        get("/logout" ,UserController::logout);
-         Database database = Database.use("mysql");
-         database.open();
-        // database.reset();
-         database.migrate();
-         database.insert("receptors", "rfc, name,taxation,street,noEx,noIn,hood,location,reference,city,state,postalCode,country,email", 
-         "GAGA940429SZ0", "Abelardo","Persona fisica","Rinconadas de la sierra del rosario","4470",""
-         ,"rinconadas de la sierra","Chihuahua","","Chihuahua","Chihuahua","31124","Mexico","Abelardogy@gmail.com");
-         RowList receptors = database.table("receptors").get();
-         for (Row row : receptors) {
-         System.out.println(row.string("name"));
-         }
-        
-         database.insert("transmitters","rfc, name, taxation,street,noEx,noIn,hood,location,reference,city,state,postalCode,country", 
-         "FADC931106995", "Cristian Manuel","Persona fisica","San ambroggio","3710","" ,"Privanza san angel","Chihuahua","","Chihuahua","Chihuahua","31107","Mexico");
-         RowList transmitters = database.table("transmitters").get();
-         for (Row transmitter : transmitters) {
-         System.out.println(transmitter.string("name")); 
-         }
-         database.close();
-         
+        get("/index", IndexController::index, new JadeTemplateEngine());
+        get("/login", UserController::login, new JadeTemplateEngine());
+        post("/login", UserController::doLogin);
+        get("/logout", UserController::logout);
+        get("/form", VoucherController::form, new JadeTemplateEngine());
+        Database database = Database.use("mysql");
+        database.open();
+        boolean empty = database.table("users").get().empty();
+        if (empty == true) {
+            database.insert("users", "user, pass", "manuel", "a123");
+        }
+        database.close();
     }
 }
--- src/main/java/com/mycompany/facturacion/controllers/VoucherController.java
+++ src/main/java/com/mycompany/facturacion/controllers/VoucherController.java
@@ -1,9 +1,23 @@
 package com.mycompany.facturacion.controllers;
 
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
 /**
  *
  * @author Cris
  */
 public class VoucherController {
     
+    public static ModelAndView form(Request rq, Response rs){
+        return new ModelAndView(new HashMap<>(), "form");
+    }
+    
+    public static Object saveForm(Request rq, Response rs){
+        String user = rq.queryParams("user");
+        
+        return null;
+    }
 }
